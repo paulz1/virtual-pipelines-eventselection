9a49f577000dd7283c47d19c024a57a2973601b6
PaulZ <paul.zakharov@gmail.com>
2021-02-18 23:28:24 +0100

    Run pytest on all tests directory (adapt cutflow_test for pytest)

--- tests/test_bins.py
+++ tests/test_bins.py
@@ -1,9 +1,10 @@
 import sys
 import ROOT
 
-f = ROOT.TFile.Open('hist_ggH.root')
+def test_bins() :
+    f = ROOT.TFile.Open('hist_ggH.root')
 
-hdata = f.Get("ggH_pt_1")
+    hdata = f.Get("ggH_pt_1")
 
-print(hdata.GetNbinsX())
-# print(len(hdata.GetNbinsX()))
+    # print(hdata.GetNbinsX())
+    assert hdata.GetNbinsX() == 30
